497f7ee4257da57c4268e944c37d7af759c67922
Jeremy Southon <southon55@gmail.com>
2019-12-17 07:25:01 +1100

    Formatted code

--- backend/backend/src/entity/Team.ts
+++ backend/backend/src/entity/Team.ts
@@ -1,44 +1,49 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne, OneToMany, ManyToMany, JoinTable, BaseEntity } from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  OneToMany,
+  ManyToMany,
+  JoinTable,
+  BaseEntity
+} from "typeorm";
 import { User } from "./User";
 import { Channel } from "./Channel";
 import { ObjectType, Field } from "type-graphql";
 
 @ObjectType()
 @Entity("teams")
-export class Team extends BaseEntity{
-
+export class Team extends BaseEntity {
   @Field()
   @PrimaryGeneratedColumn()
   id: number;
-  
+
   @Field(() => String)
   @Column()
   name: string;
 
-  
   @Field(() => User)
-  @ManyToOne(() => User, user => user.ownedTeams)
-  owner: User
+  @ManyToOne(() => User, user => user.ownedTeams, { nullable: false })
+  owner: User;
 
-  
   @Field(() => [Channel])
   @OneToMany(() => Channel, channel => channel.team)
-  channels: Channel[]
+  channels: Channel[];
 
-  
   @Field(() => [User])
   @ManyToMany(() => User)
   @JoinTable({
-      name:"teamHasUsers"
+    name: "teamHasUsers"
   })
   members: User[];
 
-  
   @Field()
   @CreateDateColumn()
   created_at: string;
 
-  
   @Field()
   @UpdateDateColumn()
   updated_at: string;
