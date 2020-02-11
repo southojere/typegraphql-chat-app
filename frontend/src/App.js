705eea0f89eb3f3891430a30269df328ea2a2acd
Jeremy <jeremy.southon@rocketlab.com.au>
2020-02-12 09:41:51 +1100

    Created new frontend - with tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,35 @@
 import React from "react";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
-
-
-import Routes from "./routes";
-import config from "./config";
-import GlobalStyles from "./styles/global";
-
-const uri = config.api;
-
-const client = new ApolloClient({
-  uri,
-  request: operation => {
-    const token = localStorage.getItem("token");
-    const refreshToken = localStorage.getItem("refreshToken");
-    operation.setContext({
-      headers: {
-        "x-authorization": token ? token : "",
-        "x-refresh-token": refreshToken ? refreshToken : ""
-      }
-    });
-  },
-  onError: ({ graphQLErrors, networkError }) => {
-    if (networkError) {
-      // logoutUser();
-    }
-  }
-});
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Routes />
-      <GlobalStyles />
-    </ApolloProvider>
+    <div className="App absolute w-full h-full flex justify-center items-center">
+      <div className="w-full max-w-md bg-grey-800">
+        <form action="" className="bg-white shadow-md rounded px-8 py-8 pt-8">
+          <div className="px-4 pb-4">
+            <label htmlFor="email" className="text-sm block font-bold pb-2">
+              Email Address
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="john@doe.com"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-700 leading-tight focus:outline-none focus:shadow-outline"
+            ></input>
+          </div>
+          <div className="px-4 pb-4">
+            <label htmlFor="password" className="text-sm block font-bold pb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-700 leading-tight focus:outline-none focus:shadow-outline"
+            ></input>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
