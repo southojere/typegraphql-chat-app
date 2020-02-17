81bf1f1b424330df44c31e693b2012699a26b7ee
Jeremy <jeremy.southon@rocketlab.com.au>
2020-02-17 18:31:44 +1100

    Added notes page

--- frontend/src/pages/Notes/index.js
+++ frontend/src/pages/Notes/index.js
@@ -1,12 +1,64 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components";
+import { Typography, Button, Icon } from "antd";
+
+const { Title } = Typography;
+
+const ME = gql`
+  query me {
+    me {
+      id
+      email
+      notes {
+        text
+      }
+    }
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const NotePadContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem 1rem;
+  max-width:400px;
+  min-width:400px;
+  margin:auto;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
+
+const ActionWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 
 const Notes = () => {
+  const { loading, error, data } = useQuery(ME);
+
+  const user = data ? data.me : null;
+  const notes = user ? user.notes : [];
   return (
-    <div className="App absolute flex justify-center items-center">
-      <div className="w-full max-w-md bg-grey-800">
-          notes
-      </div>
-    </div>
+    <Container>
+      <NotePadContainer>
+        <Title underline>Notes</Title>
+        {notes.map(note => (
+          <p>{note.text}</p>
+        ))}
+        <ActionWrapper>
+          <Button type="primary" icon="plus">
+            Add
+          </Button>
+        </ActionWrapper>
+      </NotePadContainer>
+    </Container>
   );
 };
 
